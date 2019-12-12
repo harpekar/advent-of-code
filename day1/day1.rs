0a3571856528038d8b454333cb706410e452fe6f
Karen <karenl.harper88@gmail.com>
2019-12-11 16:53:00 -0800

    Finished logic

--- day1/day1.rs
+++ day1/day1.rs
@@ -1,9 +1,23 @@
+extern crate math;
+
 use std::fs::File; 
 use std::io::prelude::*;
 use std::path::Path;
+use std::io::{self, Write};
+use math::round;
 
-fn main() { 
+fn main() {
 
-    let path = Path::new("masses.txt")
+    let total_fuel = 0; 
 
+    if let Ok(masses) = read_line("./masses.txt") {
+    
+        for mass in masses {
+            let mass_int = mass.parse::<u32>();
+            let total_fuel += (round::floor((mass_int / 3), 0) - 2)
+
+        }
+    }
+
+   print!("{}", total_fuel); 
 }
