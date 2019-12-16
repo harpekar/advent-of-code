186b189721aa048613e807ddf9515611f81db5b8
Karen <karenl.harper88@gmail.com>
2019-12-15 23:42:54 -0800

    Using File::open instead of read_lines

--- day1/day1.rs
+++ day1/day1.rs
@@ -1,22 +1,17 @@
-extern crate math;
-
 use std::fs::File; 
+use std::io::{self,BufReader};
 use std::io::prelude::*;
-use std::path::Path;
-use std::io::{self, Write};
-use math::round;
 
 fn main() {
 
     let total_fuel = 0; 
 
-    if let Ok(masses) = read_line("./masses.txt") {
-    
-        for mass in masses {
-            let mass_int = mass.parse::<u32>();
-            let total_fuel += (round::floor((mass_int / 3), 0) - 2)
+    let masses = File::open("masses.txt")?;
+    let masses = BufReader::new(masses);
 
-        }
+    for mass in masses.lines() {
+        let mass_int = mass.parse::<u32>();
+        let total_fuel = total_fuel + (mass_int / 3).floor() - 2;
     }
 
    print!("{}", total_fuel); 
